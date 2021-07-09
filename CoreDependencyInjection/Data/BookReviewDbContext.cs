03aaf3fc63d1fb9b9fb69d127b146329190ad84e
Jasper Kent <jasper_kent@hotmail.com>
2021-07-09 08:40:22 +0100

    Add project files.

---
+++ CoreDependencyInjection/Data/BookReviewDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CoreDependencyInjection.Data
+{
+    public class BookReviewDbContext : DbContext
+    {
+        public BookReviewDbContext(DbContextOptions<BookReviewDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookReview> BookReviews { get; set; }
+    }
+}
